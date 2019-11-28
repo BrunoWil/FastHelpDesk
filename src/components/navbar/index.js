ac7dc3286c14bed517d7de453be8a4dd32b1f556
Beatriz Leonel <beatriz_l.silva@hotmail.com>
2019-11-28 15:46:29 -0300

    [FH-11] Tela de Usuários Cadastrados Tecnico

---
+++ src/components/navbar/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import '../navbar/navbar.css';
+
+function Navbar(){
+    return(
+        <nav className="navbar navbar-expand-lg">
+            <span className="navbar-brand text-white text-weight-bold">FastHelp</span>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
+                <li className="nav-item active">
+                    <a className="nav-link" href="#">Home</a>
+                </li>
+
+                <button type="button" class="btn btn-dark">Exit</button>
+                
+                </ul>
+                
+            </div>
+            
+        </nav>
+    )
+}
+
+export default Navbar;
---
+++ src/view/UsuariosCadastradosTecnico/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../../components/navbar/index';
+import Navbar from '../../components/navbar/index';
+
+function UsuariosCadastrados(){
+    return(
+        <Navbar></Navbar>
+    )
+}
+
+export default UsuariosCadastrados;
